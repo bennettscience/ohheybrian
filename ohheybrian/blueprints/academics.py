ed90383b1ad0afa76cf6b01fb749b3ef7cacfadc
Brian Bennett <brian.bennett2@gmail.com>
2023-07-13 22:20:47 -0400

    Annotated transcript, small style tweaks

--- ohheybrian/blueprints/academics.py
+++ ohheybrian/blueprints/academics.py
@@ -2,25 +2,24 @@ from flask import abort, current_app, Blueprint, redirect, render_template
 from htmx_flask import make_response, request
 
 
-bp = Blueprint("academics", __name__, url_prefix='/academics')
+bp = Blueprint("academics", __name__, url_prefix="/academics")
 
 
 @bp.get("/goals")
 def get_goals():
-	return render_template('academics/partials/goals.html')
+    return render_template("academics/partials/goals.html")
+
 
 @bp.get("/academics/showcase/learning-in-out")
 def learning_in_out():
-	template = "academics/learning-in-out.html"
-	resp_data = {}
+    template = "academics/learning-in-out.html"
+    resp_data = {}
 
-	if request.htmx:
-		resp = render_template(template, **resp_data)
-	else:
-		resp = render_template(
-			"shared/layout-wrap.html",
-			partial=template,
-			data=resp_data
-		)
+    if request.htmx:
+        resp = render_template(template, **resp_data)
+    else:
+        resp = render_template(
+            "shared/layout-wrap.html", partial=template, data=resp_data
+        )
 
-	return resp
+    return resp
--- ohheybrian/blueprints/home.py
+++ ohheybrian/blueprints/home.py
@@ -5,7 +5,7 @@ from ohheybrian.models import Contact
 from ohheybrian.templates.icons import icons
 
 import nh3
- 
+
 from webargs import fields
 from webargs.flaskparser import parser
 
@@ -14,117 +14,141 @@ bp = Blueprint("home", __name__)
 
 @bp.get("/")
 def index():
-	template = "home/index.html"
-	resp_data = {}
-	
-	if request.htmx:
-		resp = render_template(template, **resp_data)
-	else:
-		resp = render_template("shared/layout-wrap.html", partial=template, data=resp_data)
+    template = "home/index.html"
+    resp_data = {}
 
-	return resp
+    if request.htmx:
+        resp = render_template(template, **resp_data)
+    else:
+        resp = render_template(
+            "shared/layout-wrap.html", partial=template, data=resp_data
+        )
+
+    return resp
+
 
 @bp.get("/about/contact")
 def contact():
-	template = "about/index.html"
-	resp_data = {}
+    template = "about/index.html"
+    resp_data = {}
 
-	if request.htmx:
-		resp = render_template(template, **resp_data)
-	else:
-		resp = render_template("shared/layout-wrap.html", partial=template, data=resp_data)
+    if request.htmx:
+        resp = render_template(template, **resp_data)
+    else:
+        resp = render_template(
+            "shared/layout-wrap.html", partial=template, data=resp_data
+        )
 
-	return resp
+    return resp
+
 
 @bp.post("/about/contact")
 def submit_contact():
-	template = "about/contact-confirm.html"
-	resp_data = {}
-	
-	args = parser.parse({
-		"last_name": fields.Str(), 
-		"first_name": fields.Str(),
-		"email": fields.Str(),
-		"message": fields.Str()
-	}, location="form")
+    template = "about/contact-confirm.html"
+    resp_data = {}
 
-	clean_text = nh3.clean(args['message'])
+    args = parser.parse(
+        {
+            "last_name": fields.Str(),
+            "first_name": fields.Str(),
+            "email": fields.Str(),
+            "message": fields.Str(),
+        },
+        location="form",
+    )
 
-	db.session.add(
-		Contact(
-			last_name=args['last_name'], 
-			first_name=args['first_name'],
-			email=args['email'],
-			message=clean_text
-		)
-	)
-	db.session.commit()
+    clean_text = nh3.clean(args["message"])
 
-	if request.htmx:
-		resp = render_template(template, **resp_data)
-	else:
-		resp = render_template("shared/layout-wrap.html", partial=template, data=resp_data)
+    db.session.add(
+        Contact(
+            last_name=args["last_name"],
+            first_name=args["first_name"],
+            email=args["email"],
+            message=clean_text,
+        )
+    )
+    db.session.commit()
 
-	return resp
+    if request.htmx:
+        resp = render_template(template, **resp_data)
+    else:
+        resp = render_template(
+            "shared/layout-wrap.html", partial=template, data=resp_data
+        )
+
+    return resp
+
 
 @bp.get("/leadership")
 def leadership():
-	template = "leadership/index.html"
-	resp_data = {}
-	
-	if request.htmx:
-		resp = render_template(template, **resp_data)
-	else:
-		resp = render_template("shared/layout-wrap.html", partial=template, data=resp_data)
+    template = "leadership/index.html"
+    resp_data = {}
 
-	return resp
+    if request.htmx:
+        resp = render_template(template, **resp_data)
+    else:
+        resp = render_template(
+            "shared/layout-wrap.html", partial=template, data=resp_data
+        )
+
+    return resp
+
 
 @bp.get("/about/resume")
 def resume():
-	template = "about/resume.html"
-	resp_data = {
-		"icons": icons
-	}
+    template = "about/resume.html"
+    resp_data = {"icons": icons}
 
-	if request.htmx:
-		resp = render_template(template, **resp_data)
-	else:
-		resp = render_template(
-			"shared/layout-wrap.html",
-			partial=template,
-			data=resp_data
-		)
+    if request.htmx:
+        resp = render_template(template, **resp_data)
+    else:
+        resp = render_template(
+            "shared/layout-wrap.html", partial=template, data=resp_data
+        )
 
-	return resp
+    return resp
+
 
 @bp.get("/academics")
 def academics():
-	template = "academics/index.html"
-	resp_data = {}
+    template = "academics/index.html"
+    resp_data = {}
 
-	if request.htmx:
-		resp = render_template(template, **resp_data)
-	else:
-		resp = render_template(
-			"shared/layout-wrap.html",
-			partial=template,
-			data=resp_data
-		)
+    if request.htmx:
+        resp = render_template(template, **resp_data)
+    else:
+        resp = render_template(
+            "shared/layout-wrap.html", partial=template, data=resp_data
+        )
 
-	return resp
+    return resp
+
 
 @bp.get("/academics/showcase")
 def showcase():
-	template = "academics/showcase.html"
-	resp_data = {}
+    template = "academics/showcase.html"
+    resp_data = {}
 
-	if request.htmx:
-		resp = render_template(template, **resp_data)
-	else:
-		resp = render_template(
-			"shared/layout-wrap.html",
-			partial=template,
-			data=resp_data
-		)
+    if request.htmx:
+        resp = render_template(template, **resp_data)
+    else:
+        resp = render_template(
+            "shared/layout-wrap.html", partial=template, data=resp_data
+        )
 
-	return resp
+    return resp
+
+
+@bp.get("/academics/transcript")
+def transcript():
+    template = "academics/transcript.html"
+    resp_data = {}
+
+    if request.htmx:
+        resp = render_template(template, **resp_data)
+    else:
+        resp = render_template(
+            "shared/layout-wrap.html", partial=template, data=resp_data
+        )
+
+    return resp
