6a6d5744f70aa923d34f6d514d5634bf69ca0737
bennettscience <brian.bennett2@gmail.com>
2023-11-22 21:40:07 -0500

    add toast script

--- ohheybrian/static/assets/script.js
+++ ohheybrian/static/assets/script.js
@@ -29,3 +29,28 @@ mouseOverContainer.onmousemove = function(e) {
 mouseOverContainer.onmouseleave = function(e) {
   el.style.transform = 'initial';
 }
+
+function showToast(msg = 'Loading...', timeout = 5000, err = false) {
+    const toast = document.querySelector(`#toast`)
+    // Handle message objects from hyperscript
+    // For non-template returns, the backend will also return JSON with
+    // the `message` key with details for the user.
+    if(typeof msg === 'object') {
+        // HTMX returns strings, so convert it to an object
+        let obj = JSON.parse(msg.xhr.responseText)
+        msg = obj.message
+    }
+
+    toast.children[0].innerText = msg;
+    if(err) {
+        toast.classList.add('error')
+    }
+    toast.classList.add('show');
+    setTimeout(() => {
+        toast.classList.remove('show')
+        toast.children[0].innerText = 'Loading...'
+        if(err) {
+            toast.classList.remove('error')
+        }
+    }, timeout)
+}
