f8f282fad55ff81abfb83c1c08011cfa43ac5bd5
bennettscience <brian.bennett2@gmail.com>
2024-01-02 21:30:35 -0500

    add toast

--- ohheybrian/static/assets/script.js
+++ ohheybrian/static/assets/script.js
@@ -1,35 +1,3 @@
-// TODO: clean up mouse tracking animation
-let constrain = 20;
-let mouseOverContainer = document.querySelector(`#cover`);
-let el = document.querySelector(`.face`);
-
-function transforms(x, y, el) {
-  let box = el.getBoundingClientRect();
-  let calcX = -(y - box.y - (box.height / 2)) / constrain;
-  let calcY = (x - box.x - (box.width / 2)) / constrain;
-  
-  return "perspective(100px) "
-    + "   rotateX("+ calcX +"deg) "
-    + "   rotateY("+ calcY +"deg) ";
-}
-
-function transformElement(el, xyEl) {
-  el.style.transform  = transforms.apply(null, xyEl);
-}
-
-mouseOverContainer.onmousemove = function(e) {
-  let xy = [e.clientX, e.clientY];
-  let position = xy.concat([el]);
-
-  window.requestAnimationFrame(function(){
-    transformElement(el, position);
-  });
-};
-
-mouseOverContainer.onmouseleave = function(e) {
-  el.style.transform = 'initial';
-}
-
 function showToast(msg = 'Loading...', timeout = 5000, err = false) {
     const toast = document.querySelector(`#toast`)
     // Handle message objects from hyperscript
@@ -54,3 +22,9 @@ function showToast(msg = 'Loading...', timeout = 5000, err = false) {
         }
     }, timeout)
 }
+
+htmx.on('showToast', evt => {
+    showToast(evt.detail.value)
+})
+
+window.showToast = showToast;
