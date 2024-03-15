3931b42d6ebd7e3fd39d7f8bd82cc3de0ef25d1d
ohheybrian <brian.bennett2@gmail.com>
2024-03-14 21:43:25 -0400

    layout templates and routes for replies

--- ohheybrian/blueprints/comments.py
+++ ohheybrian/blueprints/comments.py
@@ -4,6 +4,7 @@ from htmx_flask import make_response, request
 import nh3
 from webargs import fields
 from webargs.flaskparser import parser
+from webargs.multidictproxy import MultiDictProxy
 
 from ohheybrian.extensions import db
 from ohheybrian.models import Comment
@@ -11,64 +12,88 @@ from ohheybrian.models import Comment
 bp = Blueprint("comments", __name__)
 
 
+# Load data for mixted locations
+# https://webargs.readthedocs.io/en/latest/advanced.html
+@parser.location_loader("form_and_query")
+def load_data(request, schema):
+    newdata = request.args.copy()
+    newdata.update(request.form)
+    return MultiDictProxy(newdata, schema)
+
+
 @bp.get("/comments")
 def get_comments():
-	comments = Comment.query.filter(Comment.approved == True).all()
-	
-	return jsonify(comments)
+    comments = Comment.query.filter(Comment.approved == True).all()
+    return jsonify(comments)
+
+
+# Get a form to reply to a comment
+@bp.get("/comments/<string:slug>/reply/<int:id>")
+def get_comment_form(slug, id):
+    return render_template("shared/forms/comment-reply.html", slug=slug, comment_id=id)
+
 
 @bp.post("/comments/<string:slug>")
+@parser.use_args({"reply_to": fields.Int()}, location="query_and_form")
 def post_comment(slug):
-	args = parser.parse(
-		{
-			"name": fields.Str(),
-			"slug": fields.Str(),
+    args = parser.parse(
+        {
+            "name": fields.Str(),
+            "slug": fields.Str(),
             "url": fields.Str(),
             "message": fields.Str(),
-			"user_email": fields.Bool()
+            "user_email": fields.Bool(),
         },
-        location="form"
+        location="form",
     )
 
-	clean_text = nh3.clean(args["message"])
+    clean_text = nh3.clean(args["message"])
 
-	comment = Comment(
-		slug=args["slug"],
-		name=args["name"],
-		url=args["url"],
-		message=args["message"],
-	)
+    comment = Comment(
+        slug=args["slug"],
+        name=args["name"],
+        url=args["url"],
+        message=args["message"],
+    )
 
-	if hasattr(args, "user_email"):
-		comment.is_spam=True
+    if hasattr(args, "user_email"):
+        comment.is_spam = True
 
-	db.session.add(comment)
-	db.session.commit()
+    db.session.add(comment)
 
-	return "Thanks! All comments are moderated, so yours will appear once approved."
+    # If the comment has a reply_to key, do that now
+    if hasattr(args, "reply_to"):
+        comment.is_reply = True
+        replied_to = Comment.query.filter(Comment.id == args["reply_to"])
+        replied_to.add_reply(comment)
+
+    db.session.commit()
+
+    return "Thanks! All comments are moderated, so yours will appear once approved."
+
 
 @bp.get("/comments/<string:slug>")
 def get_post_comments(slug):
-	comments = Comment.query.filter_by(slug=slug, approved=True).order_by(Comment.occurred).all()
-	return jsonify(comments)
+    comments = (
+        Comment.query.filter_by(slug=slug, approved=True, is_reply=False)
+        .order_by(Comment.occurred)
+        .all()
+    )
+    return jsonify(comments)
 
 
 @bp.put("/comments/<int:id>")
 def moderate(id):
-	comment = Comment.query.filter(Comment.id == id).first()
-	comment.toggle_state()
-	value = "Approved" if comment.approved else "Pending"
-	return make_response(
-		value,
-		trigger={"showToast": "Comment status updated"}
-	)
+    comment = Comment.query.filter(Comment.id == id).first()
+    comment.toggle_state()
+    value = "Approved" if comment.approved else "Pending"
+    return make_response(value, trigger={"showToast": "Comment status updated"})
+
 
 @bp.delete("/comments/<int:id>")
 def delete_comment(id):
-	comment = Comment.query.filter(Comment.id == id).first()
-	db.session.delete(comment)
+    comment = Comment.query.filter(Comment.id == id).first()
+    db.session.delete(comment)
 
-	db.session.commit()
-	return make_response(
-		trigger={"showToast": "Comment deleted"}
-	)
+    db.session.commit()
+    return make_response(trigger={"showToast": "Comment deleted"})
