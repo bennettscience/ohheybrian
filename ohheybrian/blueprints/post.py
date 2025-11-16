c7a34b8f3f83cbf2fd28bb20c20251fb8b44894f
bennettscience <brian.bennett2@gmail.com>
2025-11-16 17:21:36 -0500

    Rough implement correct URL structure

--- ohheybrian/blueprints/post.py
+++ ohheybrian/blueprints/post.py
@@ -14,9 +14,10 @@ def get_posts():
     return render_template("microblog/index.html", posts=posts)
 
 
-@bp.get("/posts/<int:post_id>")
-def get_single_post(post_id):
-    post = db.session.get(Post, post_id)
+@bp.get("/posts/<int:post_year>/<int:post_month>/<string:post_slug>")
+def get_single_post(post_year, post_month, post_slug):
+    stmt = db.select(Post).where(Post.slug == post_slug)
+    post = db.session.scalars(stmt).first()
 
     if post:
         result = render_template("microblog/article.html", article=post)
