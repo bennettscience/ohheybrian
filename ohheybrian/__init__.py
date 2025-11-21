5897be54f787313f6099d76bcafbea494514427e
bennettscience <brian.bennett2@gmail.com>
2025-11-20 21:22:06 -0500

    Rename blog route for now

--- ohheybrian/__init__.py
+++ ohheybrian/__init__.py
@@ -34,7 +34,7 @@ def create_app(config):
     app.register_blueprint(category.bp)
     app.register_blueprint(comment.bp)
     app.register_blueprint(home.bp)
-    app.register_blueprint(microblog.bp, url_prefix="/micro")
+    app.register_blueprint(microblog.bp, url_prefix="/blog")
     app.register_blueprint(post.bp)
     app.register_blueprint(search.bp)
     app.register_blueprint(tag.bp)
