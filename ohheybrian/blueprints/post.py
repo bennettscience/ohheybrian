de109473e4d27065e03bfd373e8601e59ef12de8
bennettscience <brian.bennett2@gmail.com>
2025-11-22 20:50:50 -0500

    Rename posts index route

--- ohheybrian/blueprints/post.py
+++ ohheybrian/blueprints/post.py
@@ -7,15 +7,15 @@ bp = Blueprint("post", __name__)
 
 
 @bp.get("/")
-def get_posts():
+def posts_index():
     query = db.select(Post).where(Post.published).order_by(Post.created_on.desc())
     posts = db.session.scalars(query).all()
 
     return render_template("microblog/index.html", posts=posts)
 
 
-@bp.get("/posts/<int:post_year>/<int:post_month>/<string:post_slug>")
-def get_single_post(post_year, post_month, post_slug):
+@bp.get("/posts/<string:post_slug>")
+def get_single_post(post_slug):
     stmt = db.select(Post).where(Post.slug == post_slug)
     post = db.session.scalars(stmt).first()
 
