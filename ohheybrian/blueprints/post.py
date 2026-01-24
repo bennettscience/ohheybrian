5744c00e6b591db764b09e28785ab5b16dbe293f
bennettscience <brian.bennett2@gmail.com>
2026-01-24 15:34:29 -0500

    Pagination, archives
    Update pagination to use the correct URL for posts. Created an index
    page for annual archives directly that doesn't show the post body, just
    titles, reverse sorted.

--- ohheybrian/blueprints/post.py
+++ ohheybrian/blueprints/post.py
@@ -1,4 +1,7 @@
-from flask import abort, Blueprint, render_template, request
+from datetime import datetime
+
+from flask import abort, Blueprint, render_template, request, url_for
+from sqlalchemy import extract
 
 from ohheybrian.extensions import db
 from ohheybrian.models import Post
@@ -12,11 +15,35 @@ def posts_index():
     query = db.select(Post).where(Post.published).order_by(Post.created_on.desc())
     posts = db.paginate(query, page=page, per_page=20)
 
-    return render_template("microblog/index.html", pagination=posts)
+    return render_template("microblog/index.html", endpoint='post.posts_index', pagination=posts)
+
+@bp.get("/<int:year>")
+def get_posts_by_year(year):
+    page = request.args.get("page", 1, type=int)
+
+    stmt = db.select(Post).where(extract("year", Post.created_on) == year).order_by(Post.created_on.desc())
+
+    posts = db.paginate(stmt, page=page, per_page=20)
+
+    return render_template(
+        "microblog/archive.html", pagination=posts, endpoint='post.get_posts_by_year', year=year, title="All posts for {}".format(year)
+    )
+
+@bp.get("/otd")
+def on_this_day():
+    page = request.args.get("page", 1, type=int)
+    date = datetime.today()
+
+    stmt = db.select(Post).where(extract("month", Post.created_on) == date.month).where(extract("day", Post.created_on) == date.day).order_by(Post.created_on.desc())
 
 
-@bp.get("/posts/<string:post_slug>")
-def get_single_post(post_slug):
+    posts = db.paginate(stmt, page=page, per_page=20)
+
+    return render_template("microblog/index.html", pagination=posts, title="On this day...")
+
+
+@bp.get("/<string:year>/<string:month>/<string:post_slug>")
+def get_single_post(year, month, post_slug):
     stmt = db.select(Post).where(Post.slug == post_slug)
     post = db.session.scalars(stmt).first()
 
