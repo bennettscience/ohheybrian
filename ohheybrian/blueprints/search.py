cd08593ac43052ae49cc24cce4d442f109c455ff
bennettscience <brian.bennett2@gmail.com>
2026-01-25 17:25:37 -0500

    Admin search for post URLs

--- ohheybrian/blueprints/search.py
+++ ohheybrian/blueprints/search.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, url_for
 
 from ohheybrian.extensions import db
 from ohheybrian.models import Post
@@ -6,16 +6,29 @@ from ohheybrian.models import Post
 bp = Blueprint("search", __name__)
 
 
+def handle_search(param):
+    search = "%{}%".format(param)
+    stmt = db.select(Post).where(Post.title.like(search))
+    results = db.session.scalars(stmt).all()
+
+    return results
+
 @bp.get("/search")
 def search_index():
     param = request.args.get("q")
 
-    if not param:
-        results = ""
-    else:
-        search = "%{}%".format(param)
-        
-        stmt = db.select(Post).where(Post.title.like(search))
-        results = db.session.scalars(stmt).all()
+    results = handle_search(param)
 
     return render_template("microblog/search.html", results=results, query=param)
+
+@bp.post("/search")
+def search():
+    form = request.form
+    param = form.get("search")
+
+    search = handle_search(param)
+
+    results = [(post.title, url_for('post.get_single_post', post_slug=post.slug, year=post.created_year, month=post.created_month)) for post in search]
+        
+
+    return render_template("shared/partials/inline-search.html", results=results)
