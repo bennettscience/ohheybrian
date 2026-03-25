d2b62f4825863f97428276dfe4efb3eb96c74b28
bennettscience <brian.bennett2@gmail.com>
2026-03-25 18:24:19 -0400

    Search db for articles on admin page

--- ohheybrian/blueprints/search.py
+++ ohheybrian/blueprints/search.py
@@ -13,6 +13,7 @@ def handle_search(param):
 
     return results
 
+
 @bp.get("/search")
 def search_index():
     param = request.args.get("q")
@@ -21,17 +22,43 @@ def search_index():
 
     return render_template("microblog/search.html", results=results, query=param)
 
+
 @bp.post("/search")
 def search():
     form = request.form
+    origin = request.args.get("origin")
     param = form.get("search")
 
     if not param:
-        return ""
+        page = request.args.get("page", 1, type=int)
+        stmt = db.select(Post).order_by(Post.created_on.desc())
+        pagination = db.paginate(stmt, page=page, per_page=25)
 
+        results = pagination.items
+        template = "shared/partials/admin-search-result.html"
+
+    # Search the db by title only right now
+    # Return a list of post objects
     search = handle_search(param)
 
-    results = [(post.title, url_for('post.get_single_post', post_slug=post.slug, year=post.created_year, month=post.created_month)) for post in search]
-        
+    if origin:
+        # Searching from the admin page needs the full post object returned.
+        template = "shared/partials/admin-search-result.html"
+        results = search
+    else:
+        # The inline search element only needs post titles and the URL
+        template = "shared/partials/inline-search.html"
+        results = [
+            (
+                post.title,
+                url_for(
+                    "post.get_single_post",
+                    post_slug=post.slug,
+                    year=post.created_year,
+                    month=post.created_month,
+                ),
+            )
+            for post in search
+        ]
 
-    return render_template("shared/partials/inline-search.html", results=results)
+    return render_template(template, results=results)
